8023fd56321222bcd60d1ecd4686c2f05e01f79e
Stan Fortonski <contact@stanfortonski.dev>
2021-01-31 10:10:03 +0100

    Hotfix

--- routes/web-preferences.php
+++ routes/web-preferences.php
@@ -5,6 +5,6 @@ namespace Stanfortonski\Laraveltwofactor\Routes;
 use Illuminate\Support\Facades\Route;
 use Stanfortonski\Laraveltwofactor\Controllers\TwoFactorPreferencesController;
 
-Route::put('twofactor/preferences', [TwoFactorPreferencesController::class, 'index'])->name('preferences.index');
+Route::get('twofactor/preferences', [TwoFactorPreferencesController::class, 'index'])->name('preferences.index');
 Route::put('twofactor/preferences/set', [TwoFactorPreferencesController::class, 'set'])->name('preferences.set');
 Route::delete('twofactor/preferences/unset', [TwoFactorPreferencesController::class, 'unset'])->name('preferences.unset');
